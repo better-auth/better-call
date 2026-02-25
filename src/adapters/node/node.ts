35e17b397a234174dfadacb8fc1e8f7384ae73cd
Lakubu Mayanda David <Lakubudavid@gmail.com>
2026-02-25 20:43:32 +0400

    fix: proper protocol extraction from "x-forwarded-proto" header for certain edge cases

--- src/adapters/node/node.ts
+++ src/adapters/node/node.ts
@@ -5,17 +5,18 @@ import type { Router } from "../../router.js";
 
 export function toNodeHandler(handler: Router["handler"]) {
   return async (req: IncomingMessage, res: ServerResponse) => {
-    // src/adapters/node/index.ts
     const forwarded = req.headers["x-forwarded-proto"];
-    // FIXME: THERE IS A CASE WHERE FORWARDED IS AN ARRAY,
-    // IF IT'S AN ARRAY I CHECK FOR HTTPS
-    // IF NOT I DEFAULT TO HTTP
+    // THERE IS A CASE WHERE FORWARDED IS AN ARRAY, OR A COMMA SEPARATED LIST
+    // IF IT'S AN ARRAY I CHECK FOR HTTPS IN IT
     const protocol = forwarded
       ? Array.isArray(forwarded)
         ? forwarded.includes("https")
           ? "https"
           : "http"
-        : forwarded.split(",")[0].trim() // The header can also be a string with coma so I just split it manually
+        : (()=>{
+          const parts = forwarded.split(",")
+          return parts.includes("https") ? "https" : parts[0].trim() // The header can also be a string with coma so I just split it manually
+        })() 
       : (req.socket as any).encrypted
         ? "https"
         : "http";
